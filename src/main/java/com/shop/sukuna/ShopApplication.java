afead879b3a697f3bf37cecad8c5301beab34f16
quanhoang1337 <quanhoang1337@gmail.com>
2025-09-18 15:20:41 +0700

    hello controller

--- src/main/java/com/shop/sukuna/ShopApplication.java
+++ src/main/java/com/shop/sukuna/ShopApplication.java
@@ -3,7 +3,9 @@ package com.shop.sukuna;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class
+})
 public class ShopApplication {
 
 	public static void main(String[] args) {
