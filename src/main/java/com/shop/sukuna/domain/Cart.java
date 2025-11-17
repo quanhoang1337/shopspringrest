3b53b829bef3da2c60805e97413c40c7caae7170
quanhoang1337 <quanhoang1337@gmail.com>
2025-11-17 09:36:50 +0700

    updated 3 models

---
+++ src/main/java/com/shop/sukuna/domain/Cart.java
@@ -0,0 +1,37 @@
+package com.shop.sukuna.domain;
+
+import java.util.List;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.Min;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name = "carts")
+@Getter
+@Setter
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Min(value = 0)
+    private int sum;
+
+    @OneToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    @OneToMany(mappedBy = "cart")
+    List<CartDetail> cartDetails;
+
+}
--- src/main/java/com/shop/sukuna/domain/Category.java
+++ src/main/java/com/shop/sukuna/domain/Category.java
@@ -5,63 +5,43 @@ import java.util.List;
 
 import com.shop.sukuna.util.SecurityUtil;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
-import jakarta.persistence.OneToOne;
 import jakarta.persistence.PrePersist;
 import jakarta.persistence.PreUpdate;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 
 @Entity
-@Table(name = "products")
+@Table(name = "Categories")
 @Getter
 @Setter
-public class Product {
+public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
 
-    @NotBlank(message = "Tên sản phẩm không được để trống")
+    @NotBlank(message = "Tên phân loại không được để trống")
     private String name;
 
-    @NotNull(message = "Giá sản phẩm không được để trống")
-    private double price;
-
-    @NotBlank(message = "Ảnh không được để trống")
-    private String image;
-
-    @Column(columnDefinition = "MEDIUMTEXT")
-    private String detailDesc;
-
-    @Column(columnDefinition = "MEDIUMTEXT")
-    private String shortDesc;
+    @NotBlank(message = "Mô tả không được để trống")
+    private String description;
 
     private Instant createdAt;
     private Instant updatedAt;
     private String createdBy;
     private String updatedBy;
 
-    @OneToMany(mappedBy = "product")
-    private List<OrderDetail> orderDetail;
-
-    @OneToOne(mappedBy = "product")
-    private Inventory inventory;
-
-    @ManyToOne
-    @JoinColumn(name = "category_id")
-    private Category category;
+    @OneToMany(mappedBy = "category")
+    List<Product> product;
 
     @PrePersist
     public void handleBeforeCreate() {
